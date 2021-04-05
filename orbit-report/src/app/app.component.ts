2aff1e61c1c7b2595c1c7c933c1a9ee7b0e56e0f
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-04-05 14:10:18 -0500

    Step 5: Fetch Satellite Data

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -9,8 +9,25 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i <fetchedSatellites.length; i++) {
+          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+        }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this))
+    }.bind(this))
   }
+
+  
 }
